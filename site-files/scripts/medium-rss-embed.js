f50945f72609175427f6de5835370ecfe8cb9b26
Mario Davi <mariodavi@gmail.com>
2020-01-19 17:07:40 -0300

    Medium blog updated

--- site-files/scripts/medium-rss-embed.js
+++ site-files/scripts/medium-rss-embed.js
@@ -1,10 +1,10 @@
 var container = document.getElementById("retainable-rss-embed");
 if (container) {
     var css = document.createElement('link');
-    css.href = "https://www.retainable.io/assets/retainable/rss-embed/retainable.css";
+    css.href = "../styles/medium-rss-embed.css";
     css.rel = "stylesheet"
     document.getElementsByTagName('head')[0].appendChild(css);
     var script = document.createElement('script');
-    script.src = "https://www.retainable.io/assets/retainable/rss-embed/retainable.js";
+    script.src = "../scripts/medium.js";
     document.getElementsByTagName('body')[0].appendChild(script);
 }
