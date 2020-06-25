885a0f271e9edb381b196af1b19274b0065f91e5
Rafael S. L <rafael.souza.lana@gmail.com>
2020-06-25 00:07:32 -0300

    Added section 04/09 - mobile

--- src/components/home/howItWorks/howItWorks.js
+++ src/components/home/howItWorks/howItWorks.js
@@ -1,7 +1,13 @@
 import React from "react"
 import styles from "./howItWorks.module.scss"
 import initialStyles from "../initialPresentation/initial.module.scss"
-import arrowsStepsSvg from "../../../images/home/how-it-works/arrows-steps-icons.svg"
+import arrowsStepsDesktopSvg from "../../../images/home/how-it-works/arrows-steps-icons-desktop.svg"
+import step1Svg from "../../../images/home/how-it-works/step1.svg"
+import step2Svg from "../../../images/home/how-it-works/step2.svg"
+import step3Svg from "../../../images/home/how-it-works/step3.svg"
+import step4Svg from "../../../images/home/how-it-works/step4.svg"
+import step5Svg from "../../../images/home/how-it-works/step5.svg"
+import step6Svg from "../../../images/home/how-it-works/step6.svg"
 
 import { FormattedMessage } from "gatsby-plugin-intl"
 import { RowComp } from "../../bootstrap.components"
@@ -16,31 +22,52 @@ const HowItWorks = () => {
               <FormattedMessage id="home.howItWorks.title" />
             </h1>
           </RowComp>
-
           <div className={styles.steps}>
             <div className={styles.stepsLayout}>
-              <img src={arrowsStepsSvg} alt="" />
+              <img
+                src={arrowsStepsDesktopSvg}
+                className={styles.imgDesktop}
+                alt=""
+              />
               <div className={styles.descriptionsFirstRow}>
-                <p>
-                  <b>1.</b> <FormattedMessage id="home.howItWorks.step1" />
-                </p>
-                <p>
-                  <b>2.</b> <FormattedMessage id="home.howItWorks.step2" />
-                </p>
-                <p>
-                  <b>3.</b> <FormattedMessage id="home.howItWorks.step3" />
-                </p>
+                <div className={styles.descriptionItem}>
+                  <img src={step1Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>1.</b> <FormattedMessage id="home.howItWorks.step1" />
+                  </p>
+                </div>
+                <div className={styles.descriptionItem}>
+                  <img src={step2Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>2.</b> <FormattedMessage id="home.howItWorks.step2" />
+                  </p>
+                </div>
+                <div className={styles.descriptionItem}>
+                  <img src={step3Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>3.</b> <FormattedMessage id="home.howItWorks.step3" />
+                  </p>
+                </div>
               </div>
               <div className={styles.descriptionsSecondRow}>
-                <p>
-                  <b>4.</b> <FormattedMessage id="home.howItWorks.step4" />
-                </p>
-                <p>
-                  <b>5.</b> <FormattedMessage id="home.howItWorks.step5" />
-                </p>
-                <p>
-                  <b>6.</b> <FormattedMessage id="home.howItWorks.step6" />
-                </p>
+                <div className={styles.descriptionItem}>
+                  <img src={step4Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>4.</b> <FormattedMessage id="home.howItWorks.step4" />
+                  </p>
+                </div>
+                <div className={styles.descriptionItem}>
+                  <img src={step5Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>5.</b> <FormattedMessage id="home.howItWorks.step5" />
+                  </p>
+                </div>
+                <div className={styles.descriptionItem}>
+                  <img src={step6Svg} className={styles.imgMobile} alt="" />
+                  <p>
+                    <b>6.</b> <FormattedMessage id="home.howItWorks.step6" />
+                  </p>
+                </div>
               </div>
             </div>
           </div>
