00f2900c715d3dbe7cb603a2d68d3a8c163483f6
matheuscrispimcc <matheuscrispim@startupkit.cc>
2020-06-29 16:13:50 -0300

    Fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,17 +5,17 @@
  */
 
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-    if (stage === "build-html") {
-      actions.setWebpackConfig({
-        module: {
-          rules: [
-            {
-              test: /react-country-.+/,
-              use: loaders.null(),
-            },
-          ],
-        },
-      })
-    }
-  }
+// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+//     if (stage === "build-html") {
+//       actions.setWebpackConfig({
+//         module: {
+//           rules: [
+//             {
+//               test: /react-country-.+/,
+//               use: loaders.null(),
+//             },
+//           ],
+//         },
+//       })
+//     }
+//   }
