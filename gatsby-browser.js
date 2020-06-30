0c993587b2f962be712c6859fc3406f012649f81
Danilo Tuler <danilo@cartesi.io>
2020-06-30 10:18:05 -0400

    Adding lua syntax highlight

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,11 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import Prism from "prism-react-renderer/prism";
+
+(typeof global !== "undefined" ? global : window).Prism = Prism;
 
 // You can delete this file if you're not using it
 require("prismjs/themes/prism-dark.css")
+require("prismjs/components/prism-lua");
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
